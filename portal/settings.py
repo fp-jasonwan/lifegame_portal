c3a7414ad7b383b5ef37464dd8b9c863fb211b43
fp-jasonwan <75668757+fp-jasonwan@users.noreply.github.com>
2021-02-16 23:45:31 +0800

    upodated to mysql

--- portal/settings.py
+++ portal/settings.py
@@ -83,9 +83,17 @@ WSGI_APPLICATION = 'portal.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': str(BASE_DIR / 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'lifegame',
+        'USER': '18watch',
+        'PASSWORD': '18watch',
+        'HOST': '18watch.ddns.net',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': str(BASE_DIR / 'db.sqlite3'),
+    # }
 }
 
 
