5ed983dba160c8eae7726e65b60316115090affe
Jason <jason.wan@foodpanda.hk>
2021-11-09 22:55:34 +0800

    update account view

--- account/views.py
+++ account/views.py
@@ -3,14 +3,15 @@ from django.shortcuts import render, redirect
 def home_page(request):
     #you can check user here with request.user
     #example
-    if request.user.is_authenticated and request.user.is_active:
-        
-        if request.user.user_type == 'student':
-            return render(request, 'player/home.html', {})
-        elif request.user.user_type == 'oc':
-            return render(request, 'oc/home.html', {})
-        elif request.user.user_type == 'admin':
-            return render(request, 'oc/home.html', {})
-        elif request.user.user_type == 'instructor':
-            return render(request, 'oc/home.html', {})
+    if request.user:
+        if request.user.is_authenticated and request.user.is_active:
+
+            if request.user.user_type == 'student':
+                return render(request, 'player/home.html', {})
+            elif request.user.user_type == 'oc':
+                return render(request, 'oc/home.html', {})
+            elif request.user.user_type == 'admin':
+                return render(request, 'oc/home.html', {})
+            elif request.user.user_type == 'instructor':
+                return render(request, 'oc/home.html', {})
     return render(request, 'login.html', {})
