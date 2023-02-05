3325d1d7472f61e2837203ca667983f556df88d4
Jason <wanyichun@gmail.com>
2023-02-05 09:58:56 +0800

    update 20230205

--- web/stock/models.py
+++ web/stock/models.py
@@ -1,4 +1,10 @@
 from django.db import models
 
 # Create your models here.
-class Stock
+class Stock(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200)
+    lot_size = models.IntegerField()
+
+class StockPrice(models.Model):
+    stock = models.OneToOneField(StockBasic, verbose_name="股票", on_delete=models.CASCADE)
