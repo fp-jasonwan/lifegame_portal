db13016c5dde03db6f6c99418a9817585bf45b50
jwan16 <wanyichun@gmail.com>
2024-03-13 20:14:38 +0800

    fix news category

--- web/news/admin.py
+++ web/news/admin.py
@@ -2,22 +2,26 @@ from django.contrib import admin
 from .models import News, NewsCategory
 # Register your models here.
 
-class NewsInline(admin.TabularInline):
-    model = News
+# class NewsInline(admin.TabularInline):
+#     model = News
 
-class NewsAdmin(admin.ModelAdmin):
-    inlines = [NewsInline]
-    list_display = ('id', 'title', 'category')
+# class NewsAdmin(admin.ModelAdmin):
+#     inlines = [NewsInline]
+#     list_display = ('id', 'title', 'category')
 
 
-admin.site.register(News, NewsAdmin)
+admin.site.register(News
+# , NewsAdmin
+)
 # admin.site.register(NewsCategory)
 
-class NewsCategoryInline(admin.TabularInline):
-    model = NewsCategory
+# class NewsCategoryInline(admin.TabularInline):
+#     model = NewsCategory
 
-class NewsCategoryAdmin(admin.ModelAdmin):
-    inlines = [NewsCategoryInline]
-    list_display = ('id', 'name')
+# class NewsCategoryAdmin(admin.ModelAdmin):
+#     inlines = [NewsCategoryInline]
+#     list_display = ('id', 'name')
 
-admin.site.register(NewsCategory, NewsCategoryAdmin)
+admin.site.register(NewsCategory
+# , NewsCategoryAdmin
+)
