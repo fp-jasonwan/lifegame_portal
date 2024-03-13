2a634482e6e00154d2faf50cb8f8c60dd9683337
jwan16 <wanyichun@gmail.com>
2024-03-13 11:24:05 +0800

    update news

--- web/news/admin.py
+++ web/news/admin.py
@@ -2,15 +2,22 @@ from django.contrib import admin
 from .models import News, NewsCategory
 # Register your models here.
 
-admin.site.register(News)
-# admin.site.register(NewsCategory)
-
 class NewsInline(admin.TabularInline):
     model = News
-    list_display = ('id', 'name',)
 
 class NewsAdmin(admin.ModelAdmin):
     inlines = [NewsInline]
     list_display = ('id', 'title', 'category')
+
+
+admin.site.register(News, NewsAdmin)
+# admin.site.register(NewsCategory)
+
+class NewsCategoryInline(admin.TabularInline):
+    model = NewsCategory
+
+class NewsCategoryAdmin(admin.ModelAdmin):
+    inlines = [NewsCategoryInline]
+    list_display = ('id', 'name')
 
 admin.site.register(NewsCategory, NewsAdmin)
